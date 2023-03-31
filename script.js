20dd5e1620e43cc5532ef3ebd8cc2d071d0c88af
CameronTheDeveloper <cameronposey@outlook.com>
2023-03-30 19:33:17 -0700

    Add basic checkWin function

--- script.js
+++ script.js
@@ -4,18 +4,43 @@ const playGame = (boardAr) => {
     const player2 = player('O');
     const playerTurn = true;
 
-    //let result = 'play';
-    const checkWin = () => {
-
-    };
 
 
+
+};
+
+//let result = 'play';
+const checkWin = (boardAr) => {
+    //Check by row
+    for (let i = 0; i < 9; i += 3) {
+        if (boardAr[i].innerHTML !== '' && boardAr[i].innerHTML === boardAr[i + 1].innerHTML && boardAr[i + 1].innerHTML === boardAr[i + 2].innerHTML) {
+            return true;
+        }
+    }
+    //Check by column
+    for (let j = 0; j < 3; j++) {
+        if (boardAr[j].innerHTML !== '' && boardAr[j].innerHTML === boardAr[j + 3].innerHTML && boardAr[j + 3].innerHTML === boardAr[j + 6].innerHTML) {
+            return true;
+        }
+    }
+    console.log("check");
+    if (boardAr[0].innerHTML !== '' && boardAr[0].innerHTML === boardAr[4].innerHTML && boardAr[4].innerHTML === boardAr[8].innerHTML) {
+        console.log("win");
+        return true;
+    }
+
+    if (boardAr[2].innerHTML !== '' && boardAr[2].innerHTML === boardAr[4].innerHTML && boardAr[4].innerHTML === boardAr[6].innerHTML) {
+        console.log("win");
+        return true;
+    }
+    return false;
 };
 
 const board = (function () {
     const boardAr = [];
     const boardSquares = document.querySelector('#board-squares');
     let playerTurn = 'O';
+    let win = false;        //For check win
     const createBoard = () => {
         for (let i = 1; i <= 3; i++) {
             for (let j = 1; j <= 3; j++) {
@@ -24,8 +49,11 @@ const board = (function () {
                 boardSquares.appendChild(col);
                 boardAr.push(col);    //Add column to array
                 col.addEventListener('click', () => {
-                    if (col.innerHTML == '')
+                    if (col.innerHTML == '' && win == false) {
                         playerTurn = markBoard(col, playerTurn);
+                        win = checkWin(boardAr);
+                    }
+
                     //Check win
                     //
                 });
@@ -36,12 +64,14 @@ const board = (function () {
                         playerTurn = 'X';
                     }
 
-                    col.innerHTML = playerTurn;
+                    col.innerHTML = playerTurn;         //X or O
                     return playerTurn;
                 };
             }
         }
     };
+
+
     return { boardAr, createBoard };
 })();
 
@@ -55,3 +85,4 @@ const player = (symbol) => {
 
 //playGame(board.boardAr);
 board.createBoard();
+
