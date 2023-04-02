844e26ced31b8682c14ed5fe96a377503e9583e9
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-02 11:54:06 -0700

    Edit logic, playGame controls flow

--- script.js
+++ script.js
@@ -1,97 +1,114 @@
-
-const playGame = (boardAr) => {
-    const player1 = player('X');
-    const player2 = player('O');
-    const playerTurn = true;
+const playGame = () => {
+    board.createBoard();
 
 
 
+    // const player1 = player('X');
+    // const player2 = player('O');
+    // const playerTurn = true;        //Sw
 
+    //return;
+};
+
+const player = (symbol) => {
+    //let name = 
+    return {
+        symbol
+    };
 };
 
 //let result = 'play';
 const checkWin = (boardAr) => {
     //Check by row
     for (let i = 0; i < 9; i += 3) {
-        if (boardAr[i].innerHTML !== '' && boardAr[i].innerHTML === boardAr[i + 1].innerHTML && boardAr[i + 1].innerHTML === boardAr[i + 2].innerHTML) {
+        if (boardAr[i] !== '' && boardAr[i] === boardAr[i + 1] && boardAr[i + 1] === boardAr[i + 2]) {
             return true;
         }
     }
     //Check by column
     for (let j = 0; j < 3; j++) {
-        if (boardAr[j].innerHTML !== '' && boardAr[j].innerHTML === boardAr[j + 3].innerHTML && boardAr[j + 3].innerHTML === boardAr[j + 6].innerHTML) {
+        if (boardAr[j] !== '' && boardAr[j] === boardAr[j + 3] && boardAr[j + 3] === boardAr[j + 6]) {
             return true;
         }
     }
     console.log("check");
-    if (boardAr[0].innerHTML !== '' && boardAr[0].innerHTML === boardAr[4].innerHTML && boardAr[4].innerHTML === boardAr[8].innerHTML) {
+    if (boardAr[0] !== '' && boardAr[0] === boardAr[4] && boardAr[4] === boardAr[8]) {
         console.log("win");
         return true;
     }
 
-    if (boardAr[2].innerHTML !== '' && boardAr[2].innerHTML === boardAr[4].innerHTML && boardAr[4].innerHTML === boardAr[6].innerHTML) {
+    if (boardAr[2] !== '' && boardAr[2] === boardAr[4] && boardAr[4] === boardAr[6]) {
         console.log("win");
         return true;
     }
     return false;
 };
 
+
 const board = (function () {
     const boardAr = [];
     const boardSquares = document.querySelector('#board-squares');
-    const boardTitle = document.querySelector('#board-title');      // Board title
+    const boardTitle = document.querySelector('#board-title');      // Board title will display winner
     let playerTurn = 'O';
     let win = false;        //For check win
     let turnCount = 0;
+    //addSquareClickEvents(boardAr); //
     const createBoard = () => {
         for (let i = 1; i <= 3; i++) {
             for (let j = 1; j <= 3; j++) {
                 const col = document.createElement('div');
                 col.classList.add('col');
                 boardSquares.appendChild(col);
-                boardAr.push(col);    //Add column to array
-                col.addEventListener('click', () => {
-                    if (col.innerHTML == '' && win == false) {
-                        playerTurn = markBoard(col, playerTurn);
-                        win = checkWin(boardAr);
-                        turnCount++;
-                    }
-                    if (win) {        //If a player wins
-                        boardTitle.innerHTML = "Player " + "'" + playerTurn + "'" + ' Wins!';
-                        boardTitle.style.color = "#22008b";
-                        boardTitle.style.fontWeight = "700";
-                    }
-                    else if (turnCount >= 9) {
-                        boardTitle.innerHTML = 'Draw!';
-                    }
+                boardAr.push(col);    //Add column to array     -- Change this to add X or O to array. Also mark board function changes
 
-                });
-                const markBoard = (col, playerTurn) => {
-                    if (playerTurn == 'X' || playerTurn == null) {
-                        playerTurn = 'O';
-                    } else {
-                        playerTurn = 'X';
-                    }
+                // const markBoard = (col, playerTurn) => {
+                //     if (playerTurn == 'X' || playerTurn == null) {
+                //         playerTurn = 'O';
+                //     } else {
+                //         playerTurn = 'X';
+                //     }
 
-                    col.innerHTML = playerTurn;         //X or O
-                    return playerTurn;
-                };
+                //     col.innerHTML = playerTurn;         //X or O
+                //     return playerTurn;
+                // };
             }
         }
     };
 
+    const hover = () => {   //
+        console.log("Happened");
+    };
 
-    return { boardAr, createBoard };
+    const addSquareClickEvents = (boardAr) => {
+        let square = document.querySelectorAll('.col');
+        console.log("Click");
+        boardAr.forEach(function (square) {
+            square.addEventListener('click', () => {
+                if (square.innerHTML == '' && win == false) {
+                    playerTurn = markBoard(square, playerTurn);
+                    win = checkWin(boardAr);
+                    turnCount++;
+                }
+                if (win) {        //If a player wins
+                    boardTitle.innerHTML = "Player " + "'" + playerTurn + "'" + ' Wins!';
+                    boardTitle.style.color = "#22008b";
+                    boardTitle.style.fontWeight = "700";
+                }
+                else if (turnCount >= 9) {
+                    boardTitle.innerHTML = 'Draw!';
+                }
+            });
+
+        });
+    };
+
+
+    return { createBoard };
 })();
 
-const player = (symbol) => {
-    return {
-        symbol
-    };
-};
+
 
 //playGame();
 
-//playGame(board.boardAr);
-board.createBoard();
-
+//playGame(player1, player2);
+playGame();
